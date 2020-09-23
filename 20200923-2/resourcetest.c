9131f854e1a4f26ab98b112af23541fc6f8c5ebd
Hyung-Taik Choi <htcrefactor@ajou.ac.kr>
2020-09-23 15:41:36 +0900

    Make clean source file.

--- 20200923-2/resourcetest.c
+++ 20200923-2/resourcetest.c
@@ -8,12 +8,11 @@
 DeclareCounter(SysTimerCnt);
 DeclareTask(Task1);
 DeclareTask(Task2);
-DeclareEvent(event1);
-DeclareEvent(event2);
+DeclareTask(Task3);
+DeclareTask(Task4);
 
 /* Global Variables */
-int count_t1 = 0;
-int count_t2 = 0;
+
 
 /* nxtOSEK hook to be invoked from an ISR in category 2 */
 void user_1ms_isr_type2(void) {
@@ -24,43 +23,19 @@ void user_1ms_isr_type2(void) {
   	}
 }
 
+TASK(Task4) {
+	TerminateTask();
+}
+
+TASK(Task3) {
+	TerminateTask();
+}
+
 TASK(Task2) {
-	while(1) {
-		count_t2 = count_t2 + 1;
-	
-		WaitEvent(event1);
-		ClearEvent(event1);
-		nxt_motor_set_speed(A, 80, 1);
-			
-		WaitEvent(event2);
-		ClearEvent(event2);
-		nxt_motor_set_speed(A, 0, 1);
-	}
-	
 	TerminateTask();
 }
 
 TASK(Task1) {
-	count_t1 = count_t1 + 1;
-	
-	display_goto_xy(0, 1);
-	display_string("Task1 Count:");
-	display_goto_xy(12, 1);
-	display_int(count_t1, 3);
-	
-	display_goto_xy(0, 0);
-	display_string("Task2 Count:");
-	display_goto_xy(12, 0);
-	display_int(count_t2, 3);
-
-	display_update();
-	
-	if(ecrobot_get_touch_sensor(S1) == TRUE) {
-		SetEvent(Task2, event1);
-	} else {
-		SetEvent(Task2, event2);
-	}
-	
 	TerminateTask();
 }
 
