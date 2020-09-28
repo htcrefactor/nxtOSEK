eb0c999a1329ff3f00e19bbf4b689cce19fdff54
htcrefactor <htc.refactor@gmail.com>
2020-09-28 16:25:27 +0900

    Final version.

--- btslave/rxeflash.sh
+++
@@ -1,4 +0,0 @@
-echo Executing NeXTTool to upload btslave.rxe...
-wineconsole /cygdrive/C/cygwin/nexttool/NeXTTool.exe /COM=usb -download=btslave.rxe
-wineconsole /cygdrive/C/cygwin/nexttool/NeXTTool.exe /COM=usb -listfiles=btslave.rxe
-echo NeXTTool is terminated.
